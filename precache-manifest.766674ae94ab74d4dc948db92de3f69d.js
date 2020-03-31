ba866f6b99e81bf4faf719d3692c8e36a90b7f3a
Milad787878 <mohammadjavad.alghosi78@gmail.com>
2020-03-31 05:36:59 +0430

    Updates

---
+++ precache-manifest.766674ae94ab74d4dc948db92de3f69d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f161d73ab5ede5f3b6da0ddbf91a7b",
+    "url": "/Maktabkhooneh-resume/index.html"
+  },
+  {
+    "revision": "d280e79c80e2a40f2971",
+    "url": "/Maktabkhooneh-resume/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "8c40e26e687d20a2494d",
+    "url": "/Maktabkhooneh-resume/static/css/main.0e93d8b8.chunk.css"
+  },
+  {
+    "revision": "d280e79c80e2a40f2971",
+    "url": "/Maktabkhooneh-resume/static/js/2.000aa49d.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/Maktabkhooneh-resume/static/js/2.000aa49d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c40e26e687d20a2494d",
+    "url": "/Maktabkhooneh-resume/static/js/main.226b3182.chunk.js"
+  },
+  {
+    "revision": "2c40fc7699b812067305",
+    "url": "/Maktabkhooneh-resume/static/js/runtime-main.c3008744.js"
+  },
+  {
+    "revision": "ede1570fa4c88ee2226d5fe1d0daef59",
+    "url": "/Maktabkhooneh-resume/static/media/instagram.ede1570f.png"
+  },
+  {
+    "revision": "0671446988c3e36aaa13cf34df9b7b87",
+    "url": "/Maktabkhooneh-resume/static/media/js.06714469.jpg"
+  }
+]);
